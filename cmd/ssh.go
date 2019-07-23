0130105bed9329f3f16b247705231e3e8b70fb57
Samiya Akhtar <samiyaakhtar7@gmail.com>
2019-07-23 15:13:28 -0700

    Adding SSH key generation

--- cmd/ssh.go
+++ cmd/ssh.go
@@ -1,17 +1,32 @@
 package cmd
 
 import (
-	"errors"
+	// "errors"
 	"os/exec"
-
+    "io/ioutil"
 	"github.com/kyokomi/emoji"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"os"
 )
 
 // Create a cluster environment (azure simple, multi-cluster, keyvault, etc.)
-func SSH() (err error) {
-	// Make sure host system contains all utils needed by Fabrikate
+func SSH(name string) (err error) {
+	log.Info(emoji.Sprintf(":lock_with_ink_pen: Creating new SSH with name " + name))
+	currentPath, err := os.Getwd()
+    if err != nil {
+        panic(err)
+    }
+	keyPath := currentPath + "/" + name
+	log.Info(emoji.Sprintf(":gift_heart: Current wd is %s", currentPath))
+
+	if _, err := os.Stat(keyPath); err == nil {
+		log.Info(emoji.Sprintf(":palm_tree: Path to %s exists, removing...", keyPath))
+		os.Remove(keyPath)
+		os.Remove(keyPath + ".pub")
+	}
+	
+	// Make sure host system contains all utils needed by this module
 	requiredSystemTools := []string{"git", "helm", "sh", "curl", "terraform", "az"}
 	for _, tool := range requiredSystemTools {
 		path, err := exec.LookPath(tool)
@@ -23,28 +38,35 @@ func SSH() (err error) {
 
 	// Create SSH Keys
 	log.Info(emoji.Sprintf(":closed_lock_with_key: Creating New SSH Keys"))
-	if output, err := exec.Command("ssh-keygen", "-f", "bedrock/cluster/environments/bedrock-cli-demo/deploy_key", "-P", "''").CombinedOutput(); err != nil {
+	if output, err := exec.Command("ssh-keygen",  "-t", "rsa", "-b", "4096", "-f", name, "-P", "''").CombinedOutput(); err != nil {
 		log.Error(emoji.Sprintf(":no_entry_sign: %s: %s", err, output))
 		return err
 	}
 
 	if err == nil {
-		log.Info(emoji.Sprintf(":raised_hands: Cluster has been successfully created!"))
+		log.Info(emoji.Sprintf(":raised_hands: SSH key " + name + " has been created!" ))
+		log.Info(emoji.Sprintf(":pray: Add the following SSH key to 'Deploy Keys' in your Manifest repository"))
+		file, err := ioutil.ReadFile(keyPath + ".pub") 
+		if err != nil {
+			log.Error(emoji.Sprintf(":no_entry_sign: %s: %s", err, file))
+			return err
+		}
+		log.Info(string(file))
 	}
 
 	return err
 }
 
 var sshCmd = &cobra.Command{
-	Use:   "create <config>",
-	Short: "Create an Azure Kubernetes Service (AKS) cluster using Terraform",
-	Long:  `Create an Azure Kubernetes Service (AKS) cluster using Terraform`,
+	Use:   "ssh [ssh_key_name]",
+	Short: "Create an SSH key",
+	Long:  `Create an SSH key`,
 	RunE: func(cmd *cobra.Command, args []string) (err error) {
-
-		if !((args[0] == "simple") || (args[0] == "multi")) {
-			return errors.New("the environment you specified is not of the following: simple, multi, keyvault")
-		}
-		return SSH()
+		var name = "id_rsa"
+		if len(args) > 0 {
+			name = args[0]
+		} 
+		return SSH(name)
 	},
 }
 
