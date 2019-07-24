ef0090dc5b85f8d399bd2b774bd8e99c9cb3d5fc
Michael Tarng <20913254+mtarng@users.noreply.github.com>
2019-07-24 13:41:35 -0700

    Working kubeconfig merging

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -38,12 +38,20 @@ func Deploy(name string) (err error) {
 		return err
 	}
 
+	// KUBECONFIG=./output/bedrock_kube_config:~/.kube/config kubectl config view --flatten > merged-config && mv merged-config ~/.kube/config
+	// TODO: Check if a bedrock kubeconfig output exists, then add that file to the local kubeconfig.
+	log.Info(emoji.Sprintf(":mailbox_with_mail: Found Kubeconfig output. Merging into local kubeconfig."))
+	mergeConfigCmd := exec.Command("/bin/sh", "-c", "KUBECONFIG=./output/bedrock_kube_config:~/.kube/config kubectl config view --flatten > merged-config && mv merged-config ~/.kube/config")
+	mergeConfigCmd.Dir = name
+	if output, err := mergeConfigCmd.CombinedOutput(); err != nil {
+		log.Error(emoji.Sprintf(":no_entry_sign: %s: %s", err, output))
+		return err
+	}
+
 	if err == nil {
 		log.Info(emoji.Sprintf(":raised_hands: Completed Terraform environment deployment!"))
 	}
-
-	// TODO: if a bedrock kubeconfig output exists, then add that file to the local kubeconfig.
-
+	
 	return err
 }
 
