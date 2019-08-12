9c21cec99c58558fdf2012a27aabb693eccf0274
Yvonne Radsmikham <yvonne.radsmikham@gmail.com>
2019-08-12 10:36:52 -0700

    changed test cluster names to lowercase

--- cmd/simulate_test.go
+++ cmd/simulate_test.go
@@ -9,19 +9,21 @@ import (
 func TestSimulate(t *testing.T) {
 	// Runs Init Function
 	env := map[string]string{
-		"AzureSimple":   SIMPLE,
-		"AzureCommon":   COMMON,
-		"AzureSingleKV": KEYVAULT,
-		"AzureMultiple": MULTIPLE,
+		// names in lowercase because azure-multiple-clusters env requires a domain_name_label
+		//that uses only lowercase alphanumeric characters, numbers and hyphens
+		"azuresimple":   SIMPLE,
+		"azurecommon":   COMMON,
+		"azuresinglekv": KEYVAULT,
+		"azuremultiple": MULTIPLE,
 	}
 
 	for k, v := range env {
 		fmt.Println("Test simulation for environment", k)
-		err := Init(v, "test"+k)
-		if err != nil {
+		errInit := Init(v, "test-"+k)
+		if errInit != nil {
 			t.Error("There was an error creating test environment", k)
 		}
-		errSim := Simulate("bedrock/cluster/environments/test" + k)
+		errSim := Simulate("bedrock/cluster/environments/test-" + k)
 		if errSim != nil {
 			t.Error("There was an error simulating", k)
 			t.Error(errSim)
